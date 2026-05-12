31a1a39a0f2cac427cae2bb1c74acf5647aa0d66
nithin040806 <147838905+nithin040806@users.noreply.github.com>
2026-05-12 20:08:35 +0530

    Secure favourite delete with prepared statement

--- programs/removefavourite.php
+++ programs/removefavourite.php
@@ -5,8 +5,9 @@ include("function.php");
 $user_data=check_login($con);
 $a=$user_data['userid'];
 $movieid=$_SESSION['movieid'];
-$sql="DELETE from favourite where userid=$a AND movieid=$movieid";
-mysqli_query($con,$sql);
+$stmt=$con->prepare("DELETE from favourite where userid=? AND movieid=?");
+$stmt->bind_param("ss",$a,$movieid);
+$stmt->execute();
 $a=$_SESSION['pageurl'];
 header("Location:$a");
 ?>
